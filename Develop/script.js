4a62b31dbd994c5c2f56e42efffa4bf06d80964c
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-05-19 12:02:47 -0400

    implemented time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,11 @@
-// show current day
-var showDate = function() {
+// Show current day
+$("#currentDay").text(function() {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, 0);
     var mm = String(today.getMonth() + 1).padStart(2, 0);
     var yyyy = today.getFullYear();
 
-    today = mm + '/' + dd + '/' + yyyy;
-    $("#currentDay").text(today);
-}
+    return today = mm + '/' + dd + '/' + yyyy;
+})
 
-showDate();
+createTimeBlocks();
