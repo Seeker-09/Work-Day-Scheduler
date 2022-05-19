135f8704db6d89f99fcf9ee56af980a7726e5ddc
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-05-19 12:28:20 -0400

    time blocks update color coding based on real time

--- Develop/script.js
+++ Develop/script.js
@@ -8,4 +8,27 @@ $("#currentDay").text(function() {
     return today = mm + '/' + dd + '/' + yyyy;
 })
 
-createTimeBlocks();
+var updateTimeBlock = function(timeBlock, timeBlockHour) {
+    var today = new Date();
+    var hour = today.getHours();
+
+    if(timeBlockHour === hour) {
+        timeBlock.addClass("present")
+    }
+    else if(timeBlockHour < hour) {
+        timeBlock.addClass("past")
+    }
+    else {
+        timeBlock.addClass("future");
+    }
+}
+
+updateTimeBlock($("#9am"), 9);
+updateTimeBlock($("#10am"), 10);
+updateTimeBlock($("#11am"), 11);
+updateTimeBlock($("#12pm"), 12);
+updateTimeBlock($("#1pm"), 13);
+updateTimeBlock($("#2pm"), 14);
+updateTimeBlock($("#3pm"), 15);
+updateTimeBlock($("#4pm"), 16);
+updateTimeBlock($("#5pm"), 17);
